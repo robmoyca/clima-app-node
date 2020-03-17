657ecd28547e034a52f2a157b2f700a021e2304e
Roberto Moya <rmoya@tradestation.com>
2020-03-17 00:50:53 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Aplicacion del Clima - Curso de Node
+
+Recuerden ejecutar ```npm install``` para las librerias
+
+### Ejemplo:
+```
+node app -d "San Jose Costa Rica"
+```
---
+++ lugar/lugar.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(direccion) => {
+    const encodedURL = encodeURI(direccion);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedURL}`,
+        headers: { 'x-rapidapi-key': '754fe18ca6msh17233c193566e3bp12b681jsne41a598703df' }
+    });
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para ${direccion}`);
+    }
+
+    const data = resp.data.Results[0];
+    const dir = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    return {
+        dir,
+        lat,
+        lng
+    }
+}
+module.exports = {
+    getLugarLatLng
+}
